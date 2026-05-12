199da32674a52ff34823829672f7a780c927210f
Carina Schädlich <carina.schaedlich@gmail.com>
2026-05-12 11:26:46 +0200

    styling updates

--- src/components/EntryList..jsx
+++ src/components/EntryList..jsx
@@ -3,12 +3,14 @@ import EntryCard from "./EntryCard";
 export default function EntryList({ savedEntries }) {
 
   return (
+    <div className="bg-amber-100 h-screen">
     <div className="flex flex-wrap gap-5 p-4 justify-center bg-amber-100">
       {savedEntries.map((e) => {
         return (
           <EntryCard key= {e.date} e={e} />
         );
       })}
+    </div>
     </div>
   );
 }
